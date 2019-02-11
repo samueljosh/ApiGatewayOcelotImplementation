596048bb4eac8f86c93d9e5ed2ee0e4107ab6f11
samueljosh <samueljoshua@Live.com>
2019-02-11 11:16:57 -0200

    apigateway test

---
+++ OrderApi/Controllers/OrdersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderApi.Controllers
+{
+    [Route("api/orders")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        // GET api/order
+        [HttpGet("")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+         try{
+            var o1 = new Order("ID1",10);
+            var o2  = new Order("ID2",12);
+             
+            return Ok(new List<Order> {o1 ,o2} );
+         }   catch {
+
+             return BadRequest();
+         }
+
+        }
+
+
+    
+    }
+    public class  Order
+    {
+
+       public Order(string Id, Decimal Amount ){
+
+            this.Id  = Id;
+            this.Amount = Amount;
+
+       }
+
+        public string Id { get; set; }
+        public Decimal Amount { get; set; }
+
+    }
+}
